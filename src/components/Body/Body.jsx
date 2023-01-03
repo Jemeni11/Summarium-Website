ab72ad4e990b32edad342a21a2b3969a5798652a
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-01-03 18:10:02 +0100

    feat(Body): Added a new section: Supported Sites
    BREAKING CHANGE:

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import classes from "./Body.module.css";
 import DiscordMessageCustomComponent from "../DiscordMessages/Discord";
+import ScribbleHub from "../../assets/scribblehub.jpeg";
+import ArchiveOfOurOwn from "../../assets/archiveofourown.png";
+import FanFiction from "../../assets/fanfiction.jpeg";
+import FictionLive from "../../assets/fictionlive.png";
+import WebNovel from "../../assets/webnovel.jpeg";
+import SpaceBattles from "../../assets/spacebattles.png";
 
 const Body = () => {
   return (
@@ -13,6 +19,80 @@ const Body = () => {
           <DiscordMessageCustomComponent />
         </div>
       </div>
+      <div className={classes.bodySupportedSitesSection}>
+        <h2>Supported Sites</h2>
+        <SupportedSites />
+      </div>
+      <div className={classes.bodyFAQSection}>
+
+      </div>
+    </div>
+  );
+};
+
+const SupportedSites = () => {
+  const supportedSitesArray = [
+    {
+      key: 1,
+      site: "https://www.scribblehub.com/",
+      name: "ScribbleHub",
+      supports: ["stories", "author profiles"],
+      cover: ScribbleHub,
+    },
+    {
+      key: 2,
+      site: "https://www.archiveofourown.org/",
+      name: "Archive of Our Own",
+      supports: ["stories", "series", "collections"],
+      cover: ArchiveOfOurOwn,
+    },
+    {
+      key: 3,
+      site: "https://www.fanfiction.net/",
+      name: "FanFiction.net",
+      supports: ["stories"],
+      cover: FanFiction,
+    },
+    {
+      key: 4,
+      site: "https://www.fiction.live/",
+      name: "Fiction.Live",
+      supports: ["quests"],
+      cover: FictionLive,
+    },
+    {
+      key: 5,
+      site: "https://www.webnovel.com/",
+      name: "WebNovel",
+      supports: ["stories"],
+      cover: WebNovel,
+    },
+    {
+      key: 6,
+      site: "https://www.forum.spacebattles.com/",
+      name: "SpaceBattles",
+      supports: ["stories"],
+      cover: SpaceBattles,
+    },
+  ];
+
+  return (
+    <div className={classes.supportedSitesGrid}>
+      {supportedSitesArray.map((site) => (
+        <div key={site.key}>
+          <img src={site.cover} alt={site.site} />
+          <p>
+            <a href={site.site} target="_blank" referrerPolicy="no-referrer">
+              {site.name}
+            </a>
+          </p>
+          <ul>
+            {site.supports.map((support) => (
+              <li>{support.charAt(0).toUpperCase() + support.slice(1)}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
